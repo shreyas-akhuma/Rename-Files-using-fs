dcae7134c748993ca1df99dbfcf82d5810db1d84
shreyas-akhuma <67037615+shreyas-akhuma@users.noreply.github.com>
2020-07-11 19:12:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,19 +4,21 @@ const express = require('express'),
 	  bodyParser = require('body-parser'),
 	  fs = require('fs');
 
+const uploadFolder = './public/uploads/study';
+
 // Set The Storage Engine
 const storageStudy = multer.diskStorage({
-  destination : function (req, file, callback) {
-    callback(null, './public/uploads/study')
-  },
-  filename : function (req, file, callback) {
-      callback(null, file.fieldname +  Date.now() + path.extname(file.originalname));
-  }
+	destination : function (req, file, callback) {
+		callback(null, uploadFolder);
+	},
+	filename : function (req, file, callback) {
+		callback(null, file.fieldname +  Date.now() + path.extname(file.originalname));
+	}
 });
 
 // Init Upload
 const uploadStudy = multer({
-  storage : storageStudy
+	storage : storageStudy
 }).single('file');
 
 const app = express();
@@ -30,7 +32,7 @@ app.get('/', function(req, res){
 
 app.post('/study', function(req, res){
 	uploadStudy(req, res, function (err) {
-		fs.renameSync(req.file.path, req.file.path.replace('file', req.body.course));
+		fs.renameSync(req.file.path, uploadFolder + '/'+ req.body.course + Date.now() + path.extname(req.file.filename));
 	});
 	console.log(req.body, req.file);
 	res.redirect("/");
